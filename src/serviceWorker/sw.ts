1b95a78376193ace3e1d7996ce6eb6ac75117b31
alexey.gusev <gooogle.rostov@gmail.com>
2025-12-19 23:27:25 +0300

    sw update

--- src/serviceWorker/sw.ts
+++ src/serviceWorker/sw.ts
@@ -3,9 +3,17 @@
 declare const self: ServiceWorkerGlobalScope;
 
 self.addEventListener("install", (event) => {
+    // сразу активируем новую версию
     event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
-    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        (async () => {
+            // удаляем все кеши, чтобы сбросить артефакты старых версий
+            const keys = await caches.keys();
+            await Promise.all(keys.map((key) => caches.delete(key)));
+            await self.clients.claim();
+        })()
+    );
 });
